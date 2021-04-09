79f9dcc0f47030548953da88c4aaddc0b5a3664e
hieronymous-bean <jeffrey@jeffreyallanbrown.com>
2021-04-09 19:55:48 -0400

    Full Initial Sync

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import styles from './assets/styles/main.scss'
+
+createApp(App).use(router).use(store).mount('#app')
---
+++ src/views/home/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <form>
+      <input type="text" />
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Home',
+  components: {},
+}
+</script>
